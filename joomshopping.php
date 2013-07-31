4e0a9c2b77b994e117de05080f2f834d87060df0
Ben Tasker <github@bentasker.co.uk>
2013-07-31 17:03:12 +0100

    Fixed image support, added SEF for links and added View/Buy buttons

--- joomshopping.php
+++ joomshopping.php
@@ -78,14 +78,16 @@ class addonRss_joomshopping
 		}
 
 
-		$sql = "SELECT a.product_id as id, a.`name_en-GB` as title, a.`$descfield` as `desc`, a.`image` as `image`,".
+		$sql = "SELECT a.product_id as id, a.`name_en-GB` as title, a.`product_price` AS price, a.`$descfield` as `desc`, i.`image_name` as `image`,".
 			" c.category_id as catid, m.`name_en-GB` AS manufacturer ".
 			" FROM #__jshopping_products AS a ".
 			"LEFT JOIN #__jshopping_products_to_categories AS c ".
 			"ON a.product_id = c.product_id ".
 			"LEFT JOIN #__jshopping_manufacturers AS m ".
 			"ON a.product_manufacturer_id = m.manufacturer_id ".
-			"WHERE a.product_publish='1' ".
+			"LEFT JOIN #__jshopping_products_images AS i ".
+			"ON a.product_id = i.product_id " .
+			"WHERE a.product_publish='1' AND ( i.ordering=1 OR i.ordering IS NULL ) ".
 			$cats_query . $manu_query . $label_query .
 			" LIMIT {$itemCf->limit}";
     
@@ -99,7 +101,7 @@ class addonRss_joomshopping
 	
 	function getLink($row)
 	{
-		return 'index.php?option=com_jshopping&controller=product&task=view&category_id='. $row->catid."&product_id=".$row->id;
+		return JRoute::_('index.php?option=com_jshopping&controller=product&task=view&category_id='. $row->catid."&product_id=".$row->id);
 	}
 	
 	function getDesc($row,$itemCf)
@@ -115,11 +117,24 @@ class addonRss_joomshopping
 
 		$desc .='</a><br />';
 
-		if (!empty($row->manufacturer)){
-		  $desc .= "<b>Manufacturer:</b>{$row->manufacturer}<br />";
+		if ($itemCf->ShowPrice && !empty($row->price)){
+		  $desc .= "<b>Price:</b> ".money_format('%.2n',$row->price)."<br />";
+		}
+
+		if ($itemCf->ShowManu && !empty($row->manufacturer)){
+		  $desc .= "<b>Manufacturer:</b> {$row->manufacturer}<br />";
 		}
 
 		$desc .= $row->desc;
+
+		if ($itemCf->ViewButton){
+		  $desc .= "<br /><a href='".$this->getLink($row) . "'><button>View Product</button></a>";
+		}
+
+		if ($itemCf->BuyNow){
+		  $desc .= "<br /><a href='".JUri::base(false).ltrim(JRoute::_("index.php?option=com_jshopping&controller=cart&task=add&category_id={$row->catid}&product_id={$row->id}"),"/") . "'><button>Buy Now</button></a>";
+		}
+
 		return $desc;
 	}
 	
