5d54b623c49d0c10b66a8d0b6c0f655036f9f354
Ben Tasker <github@bentasker.co.uk>
2013-07-31 15:56:40 +0100

    Category filtering implemented

--- joomshopping.php
+++ joomshopping.php
@@ -12,19 +12,34 @@ class addonRss_joomshopping
 	function getItems($itemCf)
 	{
 		$db = &JFactory::getDBO();
-		
-		# clients
-		$clients 	= $itemCf->clients;
-		if (!is_array($clients)) {
-			$clients = array($clients);
+		$categories = array();
+		$cats_query = '';
+		$descfield = 'short_description_en-GB';
+
+		if (isset($_REQUEST['x'])) {
+			print_r($itemCf);
+			die;
 		}
-		if (!in_array('',$clients)) {
-			$clients = implode(',',$clients);
-			$qryClients	= " AND b.cid IN ($clients)";
-		} else {
-			$qryClients	= '';
+
+		# categories
+		$categs = $itemCf->categories;
+		if (!is_array($categs) && !empty($categs)) {
+			$categories[] = $categs;
+		}elseif(count($categs) > 0){
+			foreach ($categs as $c){
+			  $categories[] = $c;
+			}
 		}
-		
+
+		if (count($categories) > 0){
+		  $cats_query = " AND c.category_id IN (" . implode(",",$categories) .")"; 
+		}
+
+
+		if (!$itemCf->Description){
+		  $descfield = 'description_en-GB';
+		}
+	/*	
 		# categories
 		$cats 	= $itemCf->categories;
 		if (!is_array($cats)) {
@@ -37,21 +52,19 @@ class addonRss_joomshopping
 			$qryCats	= '';
 		}
 		
-		$qry = "
-			SELECT
-				b.`id` AS id, b.`name` as title, 
-				b.`params` AS `params`,
-				b.`description` as `desc`, 
-				UNIX_TIMESTAMP(b.`created`) AS s4rss_created  
-			FROM
-				`#__banners` AS b
-			WHERE
-				b.`state` = 1
-				$qryClients
-				$qryCats
-			LIMIT $itemCf->limit
-		";
-		$db->setQuery($qry);
+
+#__jshopping_products_to_categories AS c
+
+*/
+		$sql = "SELECT a.product_id as id, a.`name_en-GB` as title, a.`$descfield` as `desc`,".
+			" c.category_id as catid FROM #__jshopping_products AS a ".
+			"LEFT JOIN #__jshopping_products_to_categories AS c ".
+			"ON a.product_id = c.product_id ".
+			"WHERE a.product_publish='1' ".
+			$cats_query .
+			" LIMIT {$itemCf->limit}";
+    
+		$db->setQuery($sql);
 		$rows = $db->loadObjectList();
 		if (isset($_REQUEST['x'])) {
 			print_r($rows);#exit($qry);
@@ -61,7 +74,7 @@ class addonRss_joomshopping
 	
 	function getLink($row)
 	{
-		return 'index.php?option=com_banners&task=click&id='. $row->id;
+		return 'index.php?option=com_jshopping&controller=product&task=view&category_id='. $row->catid."&product_id=".$row->id;
 	}
 	
 	function getDesc($row,$itemCf)
